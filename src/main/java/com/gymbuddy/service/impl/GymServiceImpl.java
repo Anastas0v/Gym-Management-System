65b3712ae1de0cd71e4aed219fcf38304064c29a
Anastasov <anastasovdavid@gmail.com>
2024-11-07 23:59:05 +0100

    added basic UI

--- src/main/java/com/gymbuddy/service/impl/GymServiceImpl.java
+++ src/main/java/com/gymbuddy/service/impl/GymServiceImpl.java
@@ -1,7 +1,9 @@
 package com.gymbuddy.service.impl;
 
 import com.gymbuddy.service.GymService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GymServiceImpl implements GymService
 {
 }
--- src/main/java/com/gymbuddy/web/HomeController.java
+++ src/main/java/com/gymbuddy/web/HomeController.java
@@ -1,14 +1,16 @@
 package com.gymbuddy.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping("/home")
 public class HomeController
 {
     @GetMapping("/")
-    public String homepage()
+    public String home()
     {
-        return "Welcome to Gym asd";
+        return "home";
     }
 }
