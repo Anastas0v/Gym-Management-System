6ae8b0f81bf8b046e23e810104b1133532b14b7f
Anastasov <anastasovdavid@gmail.com>
2024-11-23 20:12:04 +0100

    MAJOR OPTIMIZATIONS AND CODE REFACTOR

--- src/main/java/com/gymbuddy/service/impl/TrainerServiceImpl.java
+++ src/main/java/com/gymbuddy/service/impl/TrainerServiceImpl.java
@@ -1,13 +1,7 @@
 package com.gymbuddy.service.impl;
 
 import com.gymbuddy.model.Trainer;
-import com.gymbuddy.model.dto.ContactDetailsDTO;
-import com.gymbuddy.model.dto.TrainerCertificationDTO;
 import com.gymbuddy.model.dto.TrainerDTO;
-import com.gymbuddy.model.enumerations.ContactType;
-import com.gymbuddy.model.enumerations.WorkingShift;
-import com.gymbuddy.model.shared.ContactDetails;
-import com.gymbuddy.model.shared.TrainerCertification;
 import com.gymbuddy.repository.ContactDetailsRepository;
 import com.gymbuddy.repository.TrainerCertificationRepository;
 import com.gymbuddy.repository.TrainerRepository;
@@ -20,13 +14,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+
+import static com.gymbuddy.service.impl.utils.TrainerDTOToClassMapper.mapProperties;
 
 @Service
 @Getter
@@ -64,100 +54,8 @@ public class TrainerServiceImpl implements TrainerService
             throw new IllegalArgumentException("The provided object is null. Please check the input data.");
         }
 
-        Trainer toSave = new Trainer();
-        mapProperties(toSave, trainerDTO);
+        Trainer toSave = mapProperties(trainerDTO, getContactDetailsRepository(), getTrainerCertificationRepository(), logger);
         return getTrainerRepository().save(toSave);
-    }
-
-    private void mapProperties(Trainer toSave, TrainerDTO trainerDTO)
-    {
-        toSave.setFirstName(trainerDTO.getFirstName());
-        toSave.setLastName(trainerDTO.getLastName());
-        toSave.setContactDetailsIds(saveAndRetrieveIds(
-                trainerDTO.getContactDetails(),
-                this::mapToContactDetails,
-                contactDetailsRepository::save));
-        toSave.setExperienceYears(trainerDTO.getExperienceYears());
-        toSave.setCertificationsIds(saveAndRetrieveIds(
-                trainerDTO.getCertifications(),
-                this::mapToTrainerCertification,
-                getTrainerCertificationRepository()::save));
-        toSave.setHourlyRate(trainerDTO.getHourlyRate());
-        toSave.setShift(WorkingShift.valueOf(trainerDTO.getShift()));
-        toSave.setBiography(trainerDTO.getBiography());
-    }
-
-    private <T, D> List<Long> saveAndRetrieveIds(List<D> dtoList, Function<D, T> mapper, Consumer<T> saver)
-    {
-        List<T> entityList = dtoList.stream()
-                .map(mapper)
-                .collect(Collectors.toList());
-
-        entityList.forEach(entity -> {
-            try
-            {
-                saver.accept(entity);
-            }
-            catch (RuntimeException e)
-            {
-                logger.error("Error saving entity: " + entity.getClass().getSimpleName(), e);
-            }
-        });
-
-        return entityList.stream()
-                .map(this::extractId)
-                .collect(Collectors.toList());
-    }
-
-    private <T> Long extractId(T entity)
-    {
-        if (entity instanceof TrainerCertification)
-        {
-            return ((TrainerCertification) entity).getId();
-        }
-        else if (entity instanceof ContactDetails)
-        {
-            return ((ContactDetails) entity).getId();
-        }
-
-        throw new IllegalArgumentException("Unsupported entity type: " + entity.getClass());
-    }
-
-    private TrainerCertification mapToTrainerCertification(TrainerCertificationDTO trainerCertificationDTO)
-    {
-        TrainerCertification trainerCertification = new TrainerCertification();
-
-        trainerCertification.setDateIssued(trainerCertificationDTO.getDateIssued());
-        trainerCertification.setValidUntil(trainerCertificationDTO.getValidUntil());
-        trainerCertification.setIssuedFrom(trainerCertificationDTO.getIssuedFrom());
-        trainerCertification.setCertificationName(trainerCertificationDTO.getCertificationName());
-        trainerCertification.setValid(checkIsValid(trainerCertificationDTO.getValidUntil()));
-
-        return trainerCertification;
-    }
-
-    private ContactDetails mapToContactDetails(ContactDetailsDTO contactDetailsDTO)
-    {
-        ContactDetails contactDetails = new ContactDetails();
-
-        contactDetails.setContact(contactDetailsDTO.getContact());
-        contactDetails.setContactType(ContactType.valueOf(contactDetailsDTO.getContactType()));
-
-        return contactDetails;
-    }
-
-    public static boolean checkIsValid(Date validUntil)
-    {
-        if (validUntil == null)
-        {
-            throw new IllegalArgumentException("The validUntil date cannot be null");
-        }
-
-        LocalDate validUntilLocalDate = validUntil.toInstant()
-                .atZone(ZoneId.systemDefault())
-                .toLocalDate();
-
-        return validUntilLocalDate.isBefore(LocalDate.now());
     }
 
     @Override
---
+++ src/main/java/com/gymbuddy/service/impl/utils/GymDTOToClassMapper.java
@@ -0,0 +1,130 @@
+package com.gymbuddy.service.impl.utils;
+
+import com.gymbuddy.model.Gym;
+import com.gymbuddy.model.dto.ContactDetailsDTO;
+import com.gymbuddy.model.dto.GymDTO;
+import com.gymbuddy.model.dto.GymLocationDTO;
+import com.gymbuddy.model.dto.WorkingInfoDTO;
+import com.gymbuddy.model.enumerations.ContactType;
+import com.gymbuddy.model.shared.ContactDetails;
+import com.gymbuddy.model.shared.GymLocation;
+import com.gymbuddy.model.shared.WorkingInfo;
+import com.gymbuddy.repository.ContactDetailsRepository;
+import com.gymbuddy.repository.GymLocationRepository;
+import com.gymbuddy.repository.WorkingInfoRepository;
+import org.slf4j.Logger;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GymDTOToClassMapper
+{
+    public static Gym mapProperties(GymDTO gymDTO, Logger logger, ContactDetailsRepository contactDetailsRepository, GymLocationRepository gymLocationRepository, WorkingInfoRepository workingInfoRepository)
+    {
+        Gym toSave = new Gym();
+        toSave.setGymName(gymDTO.getGymName());
+        toSave.setLocationId(saveAndRetrieveGymLocationId(gymDTO.getLocationDTO(), gymLocationRepository, logger));
+        toSave.setContactDetailsIds(saveAndRetrieveContactDetailsIds(gymDTO.getContactDetailsDTO(), contactDetailsRepository, logger));
+        toSave.setGymDescription(gymDTO.getGymDescription());
+        toSave.setWorkingInformationId(saveAndRetrieveWorkingInfoId(gymDTO.getWorkingInfoDTO(), workingInfoRepository, logger));
+        toSave.setGymMembership(gymDTO.getGymMembership());
+        toSave.setGym24(gymDTO.isGym24());
+        toSave.setGymCapacity(gymDTO.getGymCapacity());
+
+        return toSave;
+    }
+
+    private static Long saveAndRetrieveWorkingInfoId(WorkingInfoDTO workingInfoDTO, WorkingInfoRepository workingInfoRepository, Logger logger)
+    {
+        WorkingInfo workingInfo = mapToWorkingInfo(workingInfoDTO);
+
+        try
+        {
+            workingInfo = workingInfoRepository.save(workingInfo);
+        }
+        catch (RuntimeException e)
+        {
+            logger.error("Error saving WorkingInfo", e);
+        }
+
+        return workingInfo.getId();
+    }
+
+    private static WorkingInfo mapToWorkingInfo(WorkingInfoDTO workingInfoDTO)
+    {
+        WorkingInfo workingInfo = new WorkingInfo();
+
+        workingInfo.setClosingTime(workingInfoDTO.getClosingTime());
+        workingInfo.setOpeningTime(workingInfoDTO.getOpeningTime());
+        workingInfo.setDay(workingInfoDTO.getDay());
+
+        return workingInfo;
+    }
+
+    private static List<Long> saveAndRetrieveContactDetailsIds(List<ContactDetailsDTO> contactDetailsDTO, ContactDetailsRepository contactDetailsRepository, Logger logger)
+    {
+        List<ContactDetails> contactDetailsList = contactDetailsDTO.stream()
+                .map(GymDTOToClassMapper::mapToContactDetails)
+                .collect(Collectors.toList());
+
+        contactDetailsList.forEach(contactDetails -> {
+            try
+            {
+                contactDetailsRepository.save(contactDetails);
+            }
+            catch (RuntimeException e)
+            {
+                logger.error("Error saving ContactDetails", e);
+            }
+        });
+
+        return contactDetailsList.stream()
+                .map(ContactDetails::getId)
+                .collect(Collectors.toList());
+    }
+
+    private static ContactDetails mapToContactDetails(ContactDetailsDTO contactDetailsDTO)
+    {
+        ContactDetails contactDetails = new ContactDetails();
+
+        contactDetails.setContact(contactDetailsDTO.getContact());
+        contactDetails.setContactType(
+                Arrays.stream(ContactType.values())
+                        .filter(ct -> ct.name().equalsIgnoreCase(contactDetailsDTO.getContactType()))
+                        .findFirst()
+                        .orElseThrow(() -> new IllegalArgumentException("Invalid ContactType: " + contactDetailsDTO.getContactType()))
+        );
+
+
+        return contactDetails;
+    }
+
+    private static Long saveAndRetrieveGymLocationId(GymLocationDTO locationDTO, GymLocationRepository gymLocationRepository, Logger logger)
+    {
+        GymLocation location = mapToGymLocation(locationDTO);
+
+        try
+        {
+            location = gymLocationRepository.save(location);
+        }
+        catch (RuntimeException e)
+        {
+            logger.error("Error saving GymLocation", e);
+        }
+
+        return location.getId();
+    }
+
+    private static GymLocation mapToGymLocation(GymLocationDTO locationDTO)
+    {
+        GymLocation location = new GymLocation();
+
+        location.setGymState(locationDTO.getGymState());
+        location.setGymAddress(locationDTO.getGymAddress());
+        location.setZipCode(locationDTO.getZipCode());
+        location.setGymCity(locationDTO.getGymCity());
+
+        return location;
+    }
+}
