e0db8bbd47defd1680797014d820b358dcf42a88
Anastasov <anastasovdavid@gmail.com>
2024-11-27 22:26:02 +0100

    added tests

--- src/test/java/com/gymbuddy/service/integration/ServiceIntegrationTest.java
+++ src/test/java/com/gymbuddy/service/integration/ServiceIntegrationTest.java
@@ -2,9 +2,12 @@ package com.gymbuddy.service.integration;
 
 import com.gymbuddy.model.Gym;
 import com.gymbuddy.model.dto.GymDTO;
+import com.gymbuddy.repository.GymRepository;
 import com.gymbuddy.service.GymService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +25,17 @@ public class ServiceIntegrationTest
     @Autowired
     private GymService gymService;
 
+    @Autowired
+    private GymRepository gymRepository;
+
+    @BeforeEach
+    public void setup()
+    {
+        gymRepository.deleteAll();
+        GymDTO gymDTO = mapGym();
+        gymService.create(gymDTO);
+    }
+
     @Test
     public void createAndSaveGym_SUCCESS()
     {
@@ -31,7 +45,6 @@ public class ServiceIntegrationTest
         LocalDateTime gymCreated = truncateToMinutes(gym.getCreated());
         LocalDateTime now = truncateToMinutes(LocalDateTime.now());
 
-        // Assertion
         Assertions.assertEquals(gymCreated, now);
         Assertions.assertNotNull(gym.getId());
     }
@@ -42,7 +55,26 @@ public class ServiceIntegrationTest
         GymDTO gymDTO = mapGym();
         gymDTO.setGymName(null);
 
-        // Assertion
         Assertions.assertThrows(DataIntegrityViolationException.class, () -> gymService.create(gymDTO));
+    }
+
+    @Test
+    public void testFindById()
+    {
+        Long id = gymRepository.findAll().getFirst().getId();
+        Gym foundGym = gymService.findById(id);
+
+        Assertions.assertNotNull(foundGym);
+        Assertions.assertEquals(id, foundGym.getId());
+        Assertions.assertEquals("TestGym", foundGym.getGymName());
+    }
+
+    @Test
+    public void testDelete()
+    {
+        Long id = gymRepository.findAll().getFirst().getId();
+        gymService.delete(id);
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> gymService.findById(id));
     }
 }
